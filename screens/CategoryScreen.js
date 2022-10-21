5cd62b25294961b7af93aa65bbd4e6888aaf8409
edoka <peter.edoka.242464@unn.edu.ng>
2022-10-21 13:26:12 +0100

    splitted categories from statistics screen

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {
-    View,
-    Text,
-} from 'react-native';
+import { View } from 'react-native';
+
+import CategoryList from '../components/CategoryList';
+import RenderHeader from '../components/ScreenHeader';
+
+import { COLORS } from '../constants';
 
 export default function CategoryScreen() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>CategoryScreen</Text>
+        <View style={{ flex: 1, backgroundColor: COLORS.lightGray2, paddingTop: 20 }}>
+            <RenderHeader header={'CATEGORIES'} sub={'6 Total'} />
+            <CategoryList />
         </View>
     );
 }
