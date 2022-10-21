bddb53206e04c5121fbb62f483b010c90a5deeb2
edoka <peter.edoka.242464@unn.edu.ng>
2022-10-21 13:24:29 +0100

    created transaction screen

--- screens/TransactionScreen.js
+++ screens/TransactionScreen.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import {
     View,
-    Text,
+    FlatList,
+    SafeAreaView,
 } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+
+// components
+import CustomButton from '../components/CustomButton';
+import RenderHeader from '../components/ScreenHeader';
+
+// theme
+import { COLORS } from '../constants';
 
 export default function TransactionScreen() {
+    const type = [
+        {
+            id: 1,
+            label: "Income",
+            color: COLORS.green,
+            iconName: 'trending-up'
+        },
+        {
+            id: 2,
+            label: "Expense",
+            color: COLORS.secondary,
+            iconName: 'trending-down'
+        },
+    ]
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Add Transaction</Text>
+        <SafeAreaView style={{
+            padding: 30,
+            flex: 1,
+            height: '100%'
+        }}>
+        <View style={{
+            flex: 1,
+            backgroundColor: COLORS.lightGray2,
+            }}>
+            <RenderHeader header={'Add Transaction'} sub={'Select Transaction Type'} />
         </View>
+            <FlatList 
+                style={{padding: 50}}
+                data={type}
+                renderItem={({item}) => (<CustomButton
+                    label={item.label}
+                    backgroundColor={item.color}
+                    padding={50}
+                    width={'100%'}
+                    iconName={item.iconName}
+                    />)}
+                keyExtractor={(item) => `${item.id}`}
+            />
+        </SafeAreaView>
     );
 }
