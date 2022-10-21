3d3696a969ae1af99499874955b111bdc02c89e4
edoka <peter.edoka.242464@unn.edu.ng>
2022-10-21 13:20:35 +0100

    Added dynamic components to custom button

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -3,29 +3,49 @@ import React from 'react';
 import { Text } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 import { COLORS, FONTS } from '../constants';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-export default function CustomButton({label, onPress, justifyContent='center'}) {
+
+export default function CustomButton({
+    label='None',
+    onPress=() => {},
+    justifyContent='center',
+    textColor=COLORS.white,
+    backgroundColor=COLORS.secondary,
+    padding=20,
+    width='90%',
+    iconName=null,
+    iconColor=COLORS.white,
+    }) {
     return (
         <TouchableOpacity
             style={{
-                backgroundColor: COLORS.secondary,
-                padding: 20,
-                width: '90%',
+                backgroundColor: backgroundColor,
+                padding: padding,
+                width: width,
                 borderRadius: 10,
-                marginBottom: 8,
+                marginBottom: 30,
                 flexDirection: 'row',
                 justifyContent: justifyContent,
             }}
             onPress={onPress}>
             <Text
                 style={{
-                    color: COLORS.white,
+                    color: textColor,
                     fontSize: 18,
-                    // textAlign: '',
                     fontWeight: 'bold',
                 }}>
                 {label}
             </Text>
+            {
+                iconName != null &&
+                <MaterialIcons style={{
+                    color: iconColor,
+                    fontWeight: 'bold',
+                    marginLeft: 10,
+                    }}
+                    name={iconName} size={22}/>
+            } 
         </TouchableOpacity>
     );
 }
