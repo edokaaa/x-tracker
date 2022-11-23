3224be073bb40b063e99af692163aee37a1a6114
edoka <peter.edoka.242464@unn.edu.ng>
2022-11-23 04:14:59 +0100

    fix: updated dashboard

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,19 +1,342 @@
-import React from 'react';
-import {TouchableOpacity} from 'react-native';
-import {
-    View,
-    Text,
-} from 'react-native';
-import CustomButton from '../components/CustomButton';
+import React, {useEffect, useLayoutEffect, useState} from 'react'
+import {StyleSheet, View, TouchableOpacity} from 'react-native'
+import {Text, Avatar, ListItem} from 'react-native-elements'
+// import {auth, db} from '../firebase'
+import {StatusBar} from 'expo-status-bar'
 
-export default function DashboardScreen({navigation}) {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>DashboardScreen</Text>
-            <CustomButton
-                label={'Go to Login'}
-                onPress={() => navigation.navigate('Login')}
-            />
+import {AntDesign, Feather, FontAwesome5} from '@expo/vector-icons'
+import CustomListItem from '../components/CustomListItem'
+
+import tx from '../data/Transactions'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import { COLORS, FONTS } from '../constants';
+
+
+
+const HomeScreen = ({navigation}) => {
+  const signOutUser = () => {
+    // auth
+    //   .signOut()
+    //   .then(() => navigation.replace('Login'))
+    //   .catch((error) => alert(error.message))
+    alert("User Signed out")
+  }
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Expense Tracker',
+      headerRight: () => (
+        <View style={{marginRight: 20}}>
+          <TouchableOpacity activeOpacity={0.5} onPress={signOutUser}>
+            <Text style={{fontWeight: 'bold'}}>Logout</Text>
+          </TouchableOpacity>
         </View>
-    );
+      ),
+    })
+  }, [navigation])
+
+  // transactions
+  const [transactions, setTransactions] = useState([])
+  useEffect(() => {
+    setTransactions(tx.transactions);
+    setTotalIncome(tx.transactions.map((item) => {
+        item?.type == 'income'
+        ? 1000
+        : 0
+    }));
+    setTotalExpense(tx.transactions.map((item) => {
+        item?.type == 'expense'
+        ? 500
+        : 0
+    }));
+    
+    // const unsubscribe = db
+    //   .collection('expense')
+    //   .orderBy('timestamp', 'desc')
+    //   .onSnapshot(
+    //     (snapshot) =>
+    //       setTransactions(
+    //         snapshot.docs.map((doc) => ({
+    //           id: doc.id,
+    //           data: doc.data(),
+    //         }))
+    //       ) &
+    //       setTotalIncome(
+    //         snapshot.docs.map((doc) =>
+    //           doc.data()?.email === auth.currentUser.email &&
+    //           doc.data()?.type == 'income'
+    //             ? doc.data().price
+    //             : 0
+    //         )
+    //       ) &
+    //       setTotalExpense(
+    //         snapshot.docs.map((doc) =>
+    //           doc.data()?.email === auth.currentUser.email &&
+    //           doc.data()?.type == 'expense'
+    //             ? doc.data().price
+    //             : 0
+    //         )
+    //       )
+    //   )
+
+    // return unsubscribe
+  }, [])
+
+  // stufff
+  const [totalIncome, setTotalIncome] = useState([])
+  const [income, setIncome] = useState(0)
+  const [totalExpense, setTotalExpense] = useState([])
+  const [expense, setExpense] = useState(0)
+  const [totalBalance, setTotalBalance] = useState(0)
+  useEffect(() => {
+    if (totalIncome) {
+      if (totalIncome?.length == 0) {
+        setIncome(0)
+      } else {
+        setIncome(totalIncome?.reduce((a, b) => Number(a) + Number(b), 0))
+      }
+    }
+    if (totalExpense) {
+      if (totalExpense?.length == 0) {
+        setExpense(0)
+      } else {
+        setExpense(totalExpense?.reduce((a, b) => Number(a) + Number(b), 0))
+      }
+    }
+  }, [totalIncome, totalExpense, income, expense])
+
+  useEffect(() => {
+    if (income || expense) {
+      setTotalBalance(income - expense)
+    } else {
+      setTotalBalance(0)
+    }
+  }, [totalIncome, totalExpense, income, expense])
+
+//   const [filter, setFilter] = useState([])
+//   useEffect(() => {
+//     if (transactions) {
+//       setFilter(
+//         transactions.filter(
+//           (transaction) => transaction.data.email === auth.currentUser.email
+//         )
+//       )
+//     }
+//   }, [transactions])
+
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style='dark' />
+        <View style={styles.fullName}>
+          {/* <Avatar
+            size='medium'
+            rounded
+            source={{
+            //   uri: auth?.currentUser?.photoURL,
+            }}
+          /> */}
+          <View style={{marginLeft: 10}}>
+            <Text style={{fontWeight: 'bold', ...FONTS.h3}}>Welcome</Text>
+            <Text style={{...FONTS.largeTitle}}>
+              {/* {auth.currentUser.displayName} */}
+              {"edoka!"}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <View style={styles.cardTop}>
+            <Text style={{textAlign: 'center', color: 'aliceblue'}}>
+              Total Balance
+            </Text>
+            <Text h3 style={{textAlign: 'center', color: 'aliceblue'}}>
+              $ {totalBalance.toFixed(2)}
+            </Text>
+          </View>
+          <View style={styles.cardBottom}>
+            <View>
+              <View style={styles.cardBottomSame}>
+                <Feather name='arrow-down' size={18} color='green' />
+                <Text
+                  style={{
+                    textAlign: 'center',
+                    marginLeft: 5,
+                  }}
+                >
+                  Income
+                </Text>
+              </View>
+              <Text h4 style={{textAlign: 'center'}}>
+                {`$ ${income?.toFixed(2)}`}
+              </Text>
+            </View>
+            <View>
+              <View style={styles.cardBottomSame}>
+                <Feather name='arrow-up' size={18} color='red' />
+                <Text style={{textAlign: 'center', marginLeft: 5}}>
+                  Expense
+                </Text>
+              </View>
+              <Text h4 style={{textAlign: 'center'}}>
+                {`$ ${expense?.toFixed(2)}`}
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.recentTitle}>
+          <Text h4 style={{color: '#4A2D5D'}}>
+            Recent Transactions
+          </Text>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => navigation.navigate('Transactions')}
+          >
+            <Text style={styles.seeAll}>See All</Text>
+          </TouchableOpacity>
+        </View>
+
+        {transactions?.length > 0 ? (
+          <View style={styles.recentTransactions}>
+            {transactions?.slice(0, 3).map((info) => (
+              <View key={info.id}>
+                <CustomListItem
+                  info={info}
+                  navigation={navigation}
+                  id={info.id}
+                />
+              </View>
+            ))}
+          </View>
+        ) : (
+          <View style={styles.containerNull}>
+            <FontAwesome5 name='list-alt' size={24} color='#EF8A76' />
+            <Text h4 style={{color: '#4A2D5D'}}>
+              No Transactions
+            </Text>
+          </View>
+        )}
+      {/* </View> */}
+      <View style={styles.addButton}>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => navigation.navigate('Home')}
+        >
+          <AntDesign name='home' size={24} color='#66AFBB' />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.plusButton}
+          onPress={() => navigation.navigate('Add')}
+          activeOpacity={0.5}
+        >
+          <AntDesign name='plus' size={24} color='white' />
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => navigation.navigate('All')}
+        >
+          <FontAwesome5 name='list-alt' size={24} color='#EF8A76' />
+        </TouchableOpacity>
+      </View>
+      </SafeAreaView>
+    </>
+  )
 }
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    padding: 10,
+  },
+  fullName: {
+    flexDirection: 'row',
+  },
+  card: {
+    backgroundColor: '#535F93',
+    alignItems: 'center',
+    width: '100%',
+    padding: 10,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+    marginVertical: 20,
+  },
+  cardTop: {
+    // backgroundColor: 'blue',
+    marginBottom: 20,
+  },
+  cardBottom: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '100%',
+    margin: 'auto',
+    backgroundColor: '#E0D1EA',
+    borderRadius: 5,
+  },
+  cardBottomSame: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  recentTitle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  recentTransactions: {
+    backgroundColor: 'white',
+    width: '100%',
+  },
+  seeAll: {
+    fontWeight: 'bold',
+    color: 'green',
+    fontSize: 16,
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 0,
+    padding: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+
+    elevation: 24,
+  },
+  plusButton: {
+    backgroundColor: '#535F93',
+    padding: 10,
+    borderRadius: 50,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+    elevation: 24,
+  },
+  containerNull: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    width: '100%',
+  },
+})
